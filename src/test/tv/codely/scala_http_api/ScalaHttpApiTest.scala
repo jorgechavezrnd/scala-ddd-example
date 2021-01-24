fbec9da8678c2e01daa26700403ec9744c04a0c4
jorgechavezrnd <jorgechavezrnd@gmail.com>
2021-01-24 17:11:25 -0400

    Tests for ping end point and get all users end point, added and working

--- src/test/tv/codely/scala_http_api/ScalaHttpApiTest.scala
+++ src/test/tv/codely/scala_http_api/ScalaHttpApiTest.scala
@@ -2,17 +2,40 @@ package tv.codely.scala_http_api
 
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.testkit.ScalatestRouteTest
-import org.scalatest.{Matchers, WordSpec}
 import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.{Matchers, WordSpec}
+import spray.json._
+import tv.codely.scala_http_api.user.infrastructure.marshaller.UserMarshaller
+import tv.codely.scala_http_api.user.infrastructure.stub.UserStub
 
 final class ScalaHttpApiTest extends WordSpec with Matchers with ScalaFutures with ScalatestRouteTest {
   "ScalaHttpApi" should {
-
     "respond successfully while requesting its status" in {
       Get("/status") ~> Routes.all ~> check {
         status shouldBe StatusCodes.OK
         contentType shouldBe ContentTypes.`application/json`
         entityAs[String] shouldBe """{"status":"ok"}"""
+      }
+    }
+
+    "return pong while asking a ping" in {
+      Get("/ping") ~> Routes.all ~> check {
+        status shouldBe StatusCodes.OK
+        contentType shouldBe ContentTypes.`application/json`
+        entityAs[String] shouldBe """{"data":"pong"}"""
+      }
+    }
+
+    "return all the system users" in {
+      Get("/users") ~> Routes.all ~> check {
+        val expectedUsers = Seq(
+          UserStub(id = "deacd129-d419-4552-9bfc-0723c3c4f56a", name = "Edufasio"),
+          UserStub(id = "b62f767f-7160-4405-a4af-39ebb3635c17", name = "Edonisio")
+        )
+
+        status shouldBe StatusCodes.OK
+        contentType shouldBe ContentTypes.`application/json`
+        entityAs[String].parseJson shouldBe UserMarshaller.marshall(expectedUsers)
       }
     }
   }
