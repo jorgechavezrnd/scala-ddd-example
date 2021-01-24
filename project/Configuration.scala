1153f3ca16211e7bd7fb28186139ff76961dc377
jorgechavezrnd <jorgechavezrnd@gmail.com>
2021-01-23 22:39:58 -0400

    Configuration.scala updated

--- project/Configuration.scala
+++ project/Configuration.scala
@@ -5,11 +5,11 @@ object Configuration {
   val settings = Seq(
     organization := "tv.codely",
     scalaVersion := "2.12.8",
-
     // Custom folders path (/src/main/scala and /src/test/scala by default)
+    mainClass in Compile := Some("tv.codely.scala_http_api.ScalaHttpApi"),
     scalaSource in Compile := baseDirectory.value / "/src/main",
     scalaSource in Test := baseDirectory.value / "/src/test",
-
+    resourceDirectory in Compile := baseDirectory.value / "conf",
     // Compiler options
     scalacOptions ++= Seq(
       "-deprecation", // Warnings deprecation
@@ -18,11 +18,11 @@ object Configuration {
       "-Xlint", // More warnings when compiling
       "-Ywarn-dead-code",
       "-Ywarn-unused",
+      "-Ywarn-unused-import",
       "-Xcheckinit" // Check against early initialization
     ),
     scalacOptions in run in Compile -= "-Xcheckinit", // Remove it in production because it's expensive
     javaOptions += "-Duser.timezone=UTC",
-
     // Test options
     parallelExecution in Test := false,
     testForkedParallel in Test := false,
