169912dd80fa0a91254dd5ba4fe7ca5f83fc858e
jorgechavezrnd <jorgechavezrnd@gmail.com>
2021-01-26 17:00:54 -0400

    UsersSearcher changed to final class

--- src/main/tv/codely/scala_http_api/module/application/UsersSearcher.scala
+++ src/main/tv/codely/scala_http_api/module/application/UsersSearcher.scala
@@ -2,7 +2,7 @@ package tv.codely.scala_http_api.module.application
 
 import tv.codely.scala_http_api.module.user.domain.User
 
-class UsersSearcher {
+final class UsersSearcher {
   private val users = Seq(
     User(id = "deacd129-d419-4552-9bfc-0723c3c4f56a", name = "Edufasio"),
     User(id = "b62f767f-7160-4405-a4af-39ebb3635c17", name = "Edonisio")
