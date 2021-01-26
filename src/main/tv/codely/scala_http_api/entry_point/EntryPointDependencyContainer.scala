a80bede562b7e23adf479c0fbf72e0c69f6add0b
jorgechavezrnd <jorgechavezrnd@gmail.com>
2021-01-26 17:20:42 -0400

    User module dependency container, entry point dependency container, status get controller and user get controller, added

---
+++ src/main/tv/codely/scala_http_api/entry_point/EntryPointDependencyContainer.scala
@@ -0,0 +1,7 @@
+package tv.codely.scala_http_api.entry_point
+
+import tv.codely.scala_http_api.module.user.infrastructure.dependency_injection.UserModuleDependencyContainer
+
+final class EntryPointDependencyContainer(
+    userDependencies: UserModuleDependencyContainer
+) {}
