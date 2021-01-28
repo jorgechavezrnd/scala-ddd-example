0448c2efb22ec78ef43a75e4939ca04d02377b22
jorgechavezrnd <jorgechavezrnd@gmail.com>
2021-01-28 18:16:04 -0400

    Course Json Format Marshaller, added

---
+++ src/main/tv/codely/scala_http_api/module/courses/infrastructure/marshaller/CourseJsonFormatMarshaller.scala
@@ -0,0 +1,41 @@
+package tv.codely.scala_http_api.module.courses.infrastructure.marshaller
+
+import spray.json.DefaultJsonProtocol.jsonFormat2
+import spray.json.{DeserializationException, JsString, JsValue, JsonFormat, RootJsonFormat}
+import tv.codely.scala_http_api.module.courses.domain.{Course, CourseId, CourseName}
+
+import java.util.UUID
+
+object CourseJsonFormatMarshaller {
+
+  implicit object UuidMarshaller extends JsonFormat[UUID] {
+    def write(value: UUID): JsValue = JsString(value.toString)
+
+    def read(value: JsValue): UUID = value match {
+      case JsString(uuid) => UUID.fromString(uuid)
+      case _              => throw DeserializationException("Expected hexadecimal UUID string")
+    }
+  }
+
+  implicit object CourseIdMarshaller extends JsonFormat[CourseId] {
+    def write(value: CourseId): JsValue = JsString(value.value.toString)
+
+    def read(value: JsValue): CourseId = value match {
+      case JsString(id) => CourseId(id)
+      case _            => throw DeserializationException("Expected 1 string for CourseId")
+    }
+  }
+
+  implicit object CourseNameMarshaller extends JsonFormat[CourseName] {
+    def write(value: CourseName): JsValue = JsString(value.value)
+
+    def read(value: JsValue): CourseName = value match {
+      case JsString(name) => CourseName(name)
+      case _              => throw DeserializationException("Expected 1 string for CourseName")
+    }
+  }
+
+  implicit val courseFormat: RootJsonFormat[Course] = jsonFormat2(
+    Course.apply(_: CourseId, _: CourseName)
+  )
+}
