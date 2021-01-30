ca68aca1698f168a1f3993a4bb5ed3f70679db6d
jorgechavezrnd <jorgechavezrnd@gmail.com>
2021-01-30 14:29:22 -0400

    named parameter added in UserSpec for UserStub

--- src/test/tv/codely/scala_http_api/entry_point/UserSpec.scala
+++ src/test/tv/codely/scala_http_api/entry_point/UserSpec.scala
@@ -8,7 +8,7 @@ import tv.codely.scala_http_api.module.user.infrastructure.stub.UserStub
 final class UserSpec extends AcceptanceSpec {
   "return all the system users" in get("/users") {
     val expectedUsers = Seq(
-      UserStub(id = "deacd129-d419-4552-9bfc-0723c3c4f56a", "Edufasio"),
+      UserStub(id = "deacd129-d419-4552-9bfc-0723c3c4f56a", name = "Edufasio"),
       UserStub(id = "b62f767f-7160-4405-a4af-39ebb3635c17", name = "Edonisio")
     )
 
