153bc1af19c1e6c95df965ff7da1ca8583f6028d
jorgechavezrnd <jorgechavezrnd@gmail.com>
2021-01-31 17:43:45 -0400

    Unit Test for VideosSearcher application service, added. DurationStub and VideoCategoryStub fixed

--- src/test/tv/codely/scala_http_api/module/shared/domain/DurationStub.scala
+++ src/test/tv/codely/scala_http_api/module/shared/domain/DurationStub.scala
@@ -3,5 +3,5 @@ package tv.codely.scala_http_api.module.shared.domain
 import scala.concurrent.duration._
 
 object DurationStub {
-  def random: Duration = IntStub.randomUnsigned().minutes
+  def random: Duration = IntStub.randomUnsigned().seconds
 }
