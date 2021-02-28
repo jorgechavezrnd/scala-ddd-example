05bc7cb8f92ba0e3fbf60d56a0d1b2e4869043ec
jorgechavezrnd <jorgechavezrnd@gmail.com>
2021-02-27 21:16:43 -0400

    Some dependencies added, and 1-TowardsTypeClasses.scala file with examples, added

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,8 +2,10 @@ import sbt._
 
 object Dependencies {
   object Versions {
-    val akka     = "2.5.8"
+    val akka     = "2.5.9"
     val akkaHttp = "10.0.11"
+    val http4sVersion = "0.18.5"
+    val circeVersion = "0.9.1"
   }
 
   val production = Seq(
@@ -13,17 +15,26 @@ object Dependencies {
     "com.typesafe.akka"          %% "akka-actor"              % Versions.akka,
     "com.typesafe.akka"          %% "akka-stream"             % Versions.akka, // Explicit dependency due to: https://bit.ly/akka-http-25
     "com.typesafe.akka"          %% "akka-http-spray-json"    % Versions.akkaHttp,
-    "org.tpolecat"               %% "doobie-core"             % "0.5.0-M9",
+    "org.tpolecat"               %% "doobie-core"             % "0.5.0",
     "mysql"                      % "mysql-connector-java"     % "5.1.45",
     "com.github.scopt"           %% "scopt"                   % "3.7.0", // Command Line Commands such as de DbTablesCreator
     "com.newmotion"              %% "akka-rabbitmq"           % "5.0.0",
     "ch.qos.logback"             % "logback-classic"          % "1.2.3", // Logging backend implementation
     "com.typesafe.scala-logging" %% "scala-logging"           % "3.7.2", // SLF4J Scala wrapper
-    "net.logstash.logback"       % "logstash-logback-encoder" % "4.11" // Log JSON encoder
+    "net.logstash.logback"       % "logstash-logback-encoder" % "4.11", // Log JSON encoder
+    "org.typelevel"              %% "cats-core"               % "1.0.1",
+    "org.typelevel"              %% "cats-effect"             % "0.10",
+    "org.http4s"                 %% "http4s-dsl"              % Versions.http4sVersion,
+    "org.http4s"                 %% "http4s-blaze-server"     % Versions.http4sVersion,
+    "org.http4s"                 %% "http4s-blaze-client"     % Versions.http4sVersion,
+    "org.http4s"                 %% "http4s-circe"            % Versions.http4sVersion,
+    "io.circe"                   %% "circe-generic"           % Versions.circeVersion,
+    "io.circe"                   %% "circe-literal"           % Versions.circeVersion,
+    "com.chuusai"                %% "shapeless"               % "2.3.3"
   )
 
   val test = Seq(
-    "org.scalatest"     %% "scalatest"         % "3.0.4"           % Test,
+    "org.scalatest"     %% "scalatest"         % "3.0.5"           % Test,
     "org.scalamock"     %% "scalamock"         % "4.0.0"           % Test,
     "com.typesafe.akka" %% "akka-testkit"      % Versions.akka     % Test,
     "com.typesafe.akka" %% "akka-http-testkit" % Versions.akkaHttp % Test
